bf48ef5a1ff8f33691c4fd38e20c3c4503f58209
Crisktina <crishidalgolopez@gmail.com>
2022-10-03 23:31:58 +0200

    tasks added

---
+++ src/components/DoneTasks.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>💪DONE</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoTasks.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>💪TO-DO</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
